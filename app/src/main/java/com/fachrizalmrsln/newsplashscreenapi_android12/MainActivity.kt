d4e1a197c56e8a412d79191c654e310f96a6623f
Fachrizal <fachrizalmrsln@gmail.com>
2022-04-28 15:05:34 +0800

    re-implementing splash screen call

--- app/src/main/java/com/fachrizalmrsln/newsplashscreenapi_android12/MainActivity.kt
+++ app/src/main/java/com/fachrizalmrsln/newsplashscreenapi_android12/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.fachrizalmrsln.newsplashscreenapi_android12
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initiateSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashReady.value == true
+            }
+        }
     }
 
 }
