9ff597857459da88054f14e9c887992fc32e9795
Fachrizal <fachrizalmrsln@gmail.com>
2022-04-28 14:26:05 +0800

    trigger initiateSplashScreen() inside onCreate()

--- app/src/main/java/com/fachrizalmrsln/newsplashscreenapi_android12/MainActivity.kt
+++ app/src/main/java/com/fachrizalmrsln/newsplashscreenapi_android12/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.fachrizalmrsln.newsplashscreenapi_android12
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initiateSplashScreen()
     }
+
 }
