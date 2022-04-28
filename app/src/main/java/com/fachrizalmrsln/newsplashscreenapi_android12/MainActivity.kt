79cf05329fdb822f3762046713dd0abb30078413
Fachrizal <fachrizalmrsln@gmail.com>
2022-04-28 12:08:01 +0800

    Initial commit

---
+++ app/src/main/java/com/fachrizalmrsln/newsplashscreenapi_android12/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fachrizalmrsln.newsplashscreenapi_android12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
